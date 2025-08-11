35c9d8f556edf2342c0cec00281bc357256e66ee
RGVylar <rgvylar@gmail.com>
2025-08-11 11:04:16 +0200

    feat: /ping endpoint

---
+++ src/main/java/com/mugreparty/sprinky_server/controller/PingController.java
@@ -0,0 +1,14 @@
+package com.mugreparty.sprinky_server.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class PingController {
+    @GetMapping("/ping")    
+    public String ping() {
+        return "pong";
+    }
+    
+}

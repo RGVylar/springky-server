7d02d2169470a11dbb31680558c9193750ca2f05
RGVylar <rgvylar@gmail.com>
2025-08-11 10:51:27 +0200

    feat: bootstrap Spring Boot project

---
+++ src/main/java/com/mugreparty/sprinky_server/SprinkyServerApplication.java
@@ -0,0 +1,13 @@
+package com.mugreparty.sprinky_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinkyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinkyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mugreparty/sprinky_server/SprinkyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugreparty.sprinky_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinkyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
